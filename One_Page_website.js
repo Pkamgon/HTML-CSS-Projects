db593477c72293eb2532242251e7df13d045006a
Pkamgon <105408332+Pkamgon@users.noreply.github.com>
2023-08-31 11:47:25 -0600

    Add files via upload

--- One_Page_website.js
+++ One_Page_website.js
@@ -18,12 +18,16 @@
     }
 
     // Thumbnail image controls
+    function currentSlide(n){
+        showSlides(slideIndex = n);
+    }
+
     function showSlides(n){
     var i;
     var slides = document.getElementsByClassName("mySlides");
     var dots = document.getElementsByClassName("demo");
     var captionText = document.getElementById("caption");
-    if (n > slides.length) {slidesIndex = 1}
+    if (n > slides.length) {slideIndex = 1}
     if (n < 1) {slideIndex = slides.length}
     for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
@@ -31,9 +35,9 @@
     for (i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace(" active", "");
     }
-    slides[slidesIndex-1].style.display = "block";
-    dots[slidesIndex-1].className += " active";
-    captionText.innerHTML = dots[slidesIndex-1].alt;
+    slides[slideIndex-1].style.display = "block";
+    dots[slideIndex-1].className += " active";
+    captionText.innerHTML = dots[slideIndex-1].alt;
     }
 
         
